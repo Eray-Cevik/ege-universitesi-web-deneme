d1dc136814896131a009da0671e582bf322b1d67
Eray Çevik <123841738+Eray-Cevik@users.noreply.github.com>
2023-05-13 13:38:52 +0300

    ege-universitesi-web-deneme
    Astro.js ve bootstrap kullanarak yaptığım deneme site

---
+++ src/components/Haberler2.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+function Haberler2() {
+    return (
+        <div class="card-group ; widht:100px;">
+            <div class="card">
+                <img src="/img/ehbr2.jpg" class="card-img-top " alt="..." />
+                <div class="card-body">
+                    <h5 class="card-title">Ege Üniversitesi Daçka, Mersin Aykanat Spor’u 75-74 mağlup etti
+                    </h5>
+                    <p class="card-text">ürkiye Basketbol İkinci Ligi play- off eleme turu ikinci maçında Ege Üniversitesi Daçka, kendi evinde 
+                    ağırladığı Mersin Aykanat Spor’u 75-74 mağlup etti. Karşılaşmayı Ege Üniversitesi Büyük Spor Salonunda öğrenciler ile birlikte
+                     izleyen Rektör Prof. Dr. Necdet Budak galibiyetten ve takımın sahada verdiği mücadeleden memnuniyet duyduğunu ifade etti.</p>
+                    <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                </div>
+            </div>
+            <div class="card">
+                <img src="/img/ehbr3.jpg" class="card-img-top" alt="..." />
+                <div class="card-body">
+                    <h5 class="card-title">Prof. Dr. Uyanıkgil’den kolon ve rektum kanserlerinin tedavisine yönelik proje</h5>
+                    <p class="card-text">Ege Üniversitesi Eczacılık Fakültesi öğretim üyesi Prof. Dr. Emel Öykü Çetin Uyanıkgil’in 
+                    yürütücülüğünde, dünyada en sık görülen kanser türlerinden olan kolon ve rektum kanserlerinin tedavisine yönelik
+                     hazırlanan proje TÜBİTAK’tan destek almaya hak kazandı.</p>
+                    <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                </div>
+            </div>
+            <div class="card">
+                <img src="/img/ehbr4.jpg" class="card-img-top" alt="..." />
+                <div class="card-body">
+                    <h5 class="card-title">Ege Üniversitesinden 20 takım TEKNOFEST finalinde yarışacak</h5>
+                    <p class="card-text">ürkiye’de milli teknolojinin geliştirilmesi konusunda kritik rol oynayan birçok kuruluşun paydaşlığıyla
+                     düzenlenen Türkiye’nin ilk ve tek havacılık, uzay ve teknoloji festivali TEKNOFEST, 27 Nisan’da, İstanbul Atatürk Havalimanı’nda
+                      kapılarını açıyor. Bu yılki TEKNOFEST’e 64 takımla başvuru yapan Ege Üniversitesinde, bu takımlardan 20’si finallere gitmeye hak 
+                      kazandı. Geçen yıl düzenlenen TEKNOFEST’te Ege Üniversitesinden 3 takım finalde yer almıştı. Bu yıl finalde yarışacak takımlar, Rektör
+                       Prof. Dr. Necdet Budak’ı ziyaret etti. Prof. Dr. Budak, TEKNOFEST’te yarışacak Egeli gençlere başarı dileklerinde bulundu.</p>
+                    <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                </div>
+            </div>
+            <button type="button" class="btn btn-secondary btn-lg ">DEĞİŞTİR</button>
+        </div>
+
+
+    );
+
+}
+
+export default Haberler2;
---
+++ src/components/MyComponent.vue
@@ -0,0 +1,119 @@
+<template>
+    <br><br><br>
+    <img src="/img/okl.png" class="img-fluid" alt="..." style="margin-left: 200px;">
+    <br><br><br>
+    <div class="accordion accordion-flush" id="accordionFlushExample" style="margin-bottom: 200px;">
+        <div class="accordion-item">
+            <h2 class="accordion-header">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                    KOMİSYONLAR
+                </button>
+            </h2>
+            <div id="flush-collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                <ul class="list-group">
+                    <li class="list-group-item disabled" aria-disabled="true">Eğitim Komisyonu</li>
+                    <li class="list-group-item">Öğrenciler için Sosyal Yardım Komisyonu</li>
+                    <li class="list-group-item">Staj Komisyonu</li>
+                    <li class="list-group-item">Öğrenci Etkinlikleri Komisyonu</li>
+                    <li class="list-group-item">Kurumlar ve Sanayi İle İşbirliği Komisyonu</li>
+                </ul>
+                <br>
+                
+            </div>
+        </div>
+        <div class="accordion-item">
+            <h2 class="accordion-header">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                    OKUL FOTOĞRAFLARI
+                </button>
+            </h2>
+            <div id="flush-collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                <div id="carouselExampleIndicators" class="carousel slide" style="width: 1500px; ">
+                    <div class="carousel-indicators">
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0"
+                            class="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+                            aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+                            aria-label="Slide 3"></button>
+                    </div>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="/img/foto.jpg" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="/img/foto1.jpg" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="/img/foto2.jpg" class="d-block w-100" alt="...">
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
+                        data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
+                        data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+
+            </div>
+        </div>
+        <div class="accordion-item">
+            <h2 class="accordion-header">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+                    YÖNETİCİ KADROMUZ
+                </button>
+            </h2>
+            <div id="flush-collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                <div class="card" style="width: 18rem; float: left; margin-left: 100px; margin-right: 50px; ">
+                    <img src="/img/müdür.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Dr. Öğr. üyesi Nedim ÇETİNKAYA</h5>
+                        <h6>Meslek Yüksekokulu Müdürü</h6>
+                    </div>
+                </div>
+                <div class="card" style="width: 18rem; float: left; margin-left: 50px;">
+                    <img src="/img/turkler.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Öğr. Gör. Levent TüRKLER</h5>
+                        <h6>Meslek Yüksekokulu Müdür Yardımcısı</h6>
+                    </div>
+                </div>
+                <div class="card" style="width: 18rem; float: left; margin-left: 50px;">
+                    <img src="/img/müdüry.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Öğr. Gör. Dr. Mehmet ÇETİN</h5>
+                        <h6>Meslek Yüksekokulu Müdür Yardımcısı</h6>
+                    </div>
+                </div>
+                <div class="card" style="width: 18rem; float: left; margin-left: 50px;">
+                    <img src="/img/yaramis.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Seher YARAMIŞ</h5>
+                        <h6>Yüksekokul Sekreter V.</h6>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: "MyComponent",
+    props: {
+        
+        title: String
+    }
+}
+</script>
